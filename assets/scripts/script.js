e29ecd306bdd129191035010d8c969d5cf185854
sigmamix10 <sigmamix10@gmail.com>
2023-02-19 19:39:23 -0300

    Autalizações no projeto

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,28 +3,29 @@ function handleMouseEnter() {
   document.body.id = `${this.id}-hovered`;
 }
 
-function handleMouseLeave() {
+function handleMouseLeave(){
   this.classList.remove('s-card--hovered');
   document.body.id = '';
 }
 
-function addEventListenersToCards() {
+function addEventListenersToCards(){
   const cardElements = document.getElementsByClassName('s-card');
-  
-  for (let index = 0; index < cardElements.length; index++) {
-    const card = cardElements[index];
-    card.addEventListener('mouseenter', handleMouseEnter);
-    card.addEventListener('mouseleave', handleMouseLeave);
+
+  for(let index = 0; index < cardElements.length; index++){
+      const card = cardElements[index];
+      card.addEventListener('mouseenter', handleMouseEnter);
+      card.addEventListener('mouseleave', handleMouseLeave);
   }
 }
 
 document.addEventListener("DOMContentLoaded", addEventListenersToCards, false);
 
-function selectCarouselItem(selectedButtonElement) {
-  const selectedItem = selectedButtonElement.id;
-  const carousel = document.querySelector('.s-cards-carousel');
+function selectCarouselItem (selectedButtomElement){
+  const selectedItem = selectedButtomElement.id;
+  const carousel = document.querySelector('.s-card-carousel');
   const transform = carousel.style.transform;
   const rotateY = transform.match(/rotateY\((-?\d+deg)\)/i);
+
   const rotateYDeg = -120 * (Number(selectedItem) - 1);
   const newTransform = transform.replace(rotateY[0], `rotateY(${rotateYDeg}deg)`);
 
@@ -32,5 +33,5 @@ function selectCarouselItem(selectedButtonElement) {
 
   const activeButtonElement = document.querySelector('.s-controller__button--active');
   activeButtonElement.classList.remove('s-controller__button--active');
-  selectedButtonElement.classList.add('s-controller__button--active');
+  selectedButtomElement.classList.add('s-controller__button--active');
 }
